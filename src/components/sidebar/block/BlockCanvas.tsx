4916e868e59091a50da42153b8baada2c20cfd02
Yu Shi Jie <yshijie1999@gmail.com>
2023-05-23 14:49:51 +0800

    feat: implemented keydown selection for pixel colors during block mint

--- src/components/sidebar/block/BlockCanvas.tsx
+++ src/components/sidebar/block/BlockCanvas.tsx
@@ -5,7 +5,6 @@ import { ColorResult } from "@hello-pangea/color-picker";
 const BlockCanvas = ({ colors, selectedColor, onCellClick = () => { }, editable = false }:
   { colors: `#${string}`[], selectedColor?: `#${string}`, onCellClick?: Function, editable?: boolean }) => {
 
-
   function handleCellClick(e: React.MouseEvent<HTMLDivElement, MouseEvent>, i: number) {
     e.preventDefault()
     if (editable) {
@@ -13,10 +12,20 @@ const BlockCanvas = ({ colors, selectedColor, onCellClick = () => { }, editable 
     }
   }
 
+  function handleCellMouseDown(e: React.MouseEvent<HTMLDivElement, MouseEvent>, i: number) {
+    e.preventDefault()
+    if (e.buttons === 1 || e.buttons === 3 || e.shiftKey) {
+      if (editable) {
+        onCellClick(i, selectedColor)
+      }
+    }
+
+  }
+
   return (
     <SimpleGrid columns={10} border={"3px solid gray"}>
       {
-        colors.map((color, i) => <Box border={"1px solid gray"} key={i} bg={color} height='32px' width='32px' onClick={(e) => handleCellClick(e, i)} />)
+        colors.map((color, i) => <Box border={"1px solid gray"} key={i} bg={color} height='32px' width='32px' onMouseEnter={(e) => handleCellMouseDown(e, i)} onClick={(e) => handleCellClick(e, i)} />)
       }
     </SimpleGrid>)
 }
